3db6535c1a2e23a75edad82c8a3f5b83a5725b35
thathsaranandun <46743657+thathsaranandun@users.noreply.github.com>
2019-02-01 10:04:40 +0530

    updated map.ts
    Added popup window for occupied station marker and added marker for closed station.

--- map/src/pages/map/map.ts
+++ map/src/pages/map/map.ts
@@ -43,7 +43,7 @@ export class MapPage {
     })
 
     var markerWindow = new google.maps.InfoWindow({
-      content:'<h4>Osaka Charging Station</h4><br><p>Port Type:J1770<br>Availability: Available<br>Open Time:24/7</p>'
+      content:'<h4>Charging Station 1</h4><br><p>Port Type:J1770<br>Availability: Available<br>Open Time:24/7</p>'
     })
 
     marker.addListener('click',function(){
@@ -57,6 +57,32 @@ export class MapPage {
       icon:'../assets/img/map_marker_icon_1 - Copy.png'
 
     })
+
+    var markerWindow2 = new google.maps.InfoWindow({
+      content:'<h4>Charging station 2</h4><br><p>Port Type:J1770<br>Availability: Occupied<br>Open Time:24/7</p>'
+    })
+
+    marker2.addListener('click',function(){
+      markerWindow2.open(this.map,marker2)
+
+    })
+
+    let marker3 : google.maps.Marker = new google.maps.Marker({
+      map:this.map,
+      position:{lat:6.9395,lng:79.8781},
+      icon:'../assets/img/map_marker_icon_2 - Copy.png'
+
+    })
+
+    var markerWindow3 = new google.maps.InfoWindow({
+      content:'<h4>Charging station 3</h4><br><p>Port Type:J1770<br>Availability: Closed<br>Open Time:24/7</p>'
+    })
+
+    marker3.addListener('click',function(){
+      markerWindow3.open(this.map,marker3)
+
+    })
+
   }
 
 
