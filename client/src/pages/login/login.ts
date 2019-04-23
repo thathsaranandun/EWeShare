a4fbc8d03b1137fd0559f27b70ea9170c80c49da
thathsaranandun <46743657+thathsaranandun@users.noreply.github.com>
2019-04-23 15:41:13 +0530

    created login client-server functionalities

--- client/src/pages/login/login.ts
+++ client/src/pages/login/login.ts
@@ -1,7 +1,9 @@
 import { SignupPage } from './../signup/signup';
 import { TabsPage } from './../tabs/tabs';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { DataService } from '../../app/services/data.service';
+import { MapPage } from '../map/map';
 
 
 @IonicPage()
@@ -13,12 +15,55 @@ export class Login {
 
   nextPage =TabsPage;
   signPage=SignupPage;
+  username:string='';
+  password:string='';
+  enteredDataStatus:boolean=false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public dataService:DataService,public alertCtrl:AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Login');
   }
+
+  loginUser(){
+    if(/^[a-zA-Z0-9]+$/.test(this.username)){
+      //Validate
+      this.dataService.postLogIn(this.username,this.password).subscribe((data:boolean) => {
+        console.log(data);
+        this.enteredDataStatus=data;
+        console.log('enteredDataStatus:'+this.enteredDataStatus)
+        if(this.enteredDataStatus==true){
+          this.navCtrl.push(MapPage,{
+            username:this.username
+          });
+  
+        }else{
+          this.alert('Error','Invalid Login details. Please enter again.');
+          this.username='';
+          this.password='';
+          console.log('cant load chat page');
+        }
+
+      })
+
+      
+       
+
+    }else{
+      this.alert('Error','Invalid Login details. Please enter again.');
+    }
+
+  }
+
+  alert(title:string,message:string){
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: message,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
 
 }
