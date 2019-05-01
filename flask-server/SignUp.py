4bd7267c57357425b55553969b31bab8c597338f
Sanka Chinthana <sanka.2017357@iit.ac.lk>
2019-05-02 01:42:29 +0530

    signup validation

--- flask-server/SignUp.py
+++ flask-server/SignUp.py
@@ -1,3 +1,4 @@
+import flask
 from flask import request
 
 import DatabaseConnection
@@ -22,16 +23,32 @@ class SignUp:
 
         con = DatabaseConnection.connectdb()
         cur = con.cursor()
-        cur.execute(
-            """INSERT INTO 
-                users (
-                    userFName,
-                    userLName,
-                    email,
-                    username,
-                    address,
-                    password)
-            VALUES (%s,%s,%s,%s,%s,%s)""",
-            (self.fname, self.lname, self.username, self.email, self.address, self.password))
-        con.commit()
-        return 'User Registration Successful.'
+        cur.execute("SELECT COUNT(1) FROM users WHERE username = %s;", [self.username])
+        row_count = cur.rowcount
+        if row_count == 1:  #checking the usernam exist
+            return 'Username is already exists'
+        else:
+            if len(self.password) <= 6: #check password legth
+                return 'Password is too short'
+            else:
+                j=0
+                for i in range(0 ,len(self.email)):
+                    if self.email[i] == '@':
+                        j=1
+                if j ==1:   #email validation
+                    cur.execute(
+                    """INSERT INTO
+                        users (
+                            userFName,
+                            userLName,
+                            email,
+                            username,
+                            address,
+                            password)
+                    VALUES (%s,%s,%s,%s,%s,%s)""",
+                    (self.fname, self.lname, self.username, self.email, self.address, self.password))
+                    con.commit()
+                    return 'User Registration Successful.'
+                else:
+                    return 'invalid email address'
+
