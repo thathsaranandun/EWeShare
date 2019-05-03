40fe8b90ce83197970a8f50c9182ec5cb876c5fe
Sanka Chinthana <sanka.2017357@iit.ac.lk>
2019-05-03 19:31:17 +0530

    gitignore update

--- flask-server/DatabaseConnection.py
+++ flask-server/DatabaseConnection.py
@@ -1,15 +1,8 @@
 import pymysql
-from pymysql.connections import Connection
 
-host='localhost'
+host = 'localhost'
 user = 'root'
 password = ''
 db = 'sdgpdb'
 
-
-connection = pymysql.connect(host=host,
-                                             user=user,
-                                             password=password,
-                                             db=db,
-                                             charset='utf8',
-                                             cursorclass=pymysql.cursors.DictCursor)
+connection = pymysql.connect(host=host, user=user, password=password, db=db, charset='utf8', cursorclass=pymysql.cursors.DictCursor)
