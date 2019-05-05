99f26f74f7792500011d8e94cd6ae4ad320683e3
Geeth Kodikara <gvkodikara1@gmail.com>
2019-05-05 17:55:01 +0530

    SignUp.py VALIDATIONS were included

--- flask-server/SignUp.py
+++ flask-server/SignUp.py
@@ -15,20 +15,31 @@ class SignUp:
         self.password = password
 
     def signup(self):
+        message = SignUp.validateSignup(self)
+        if(message['supValid']):
+            con = DatabaseConnection.connection
+            cur = con.cursor()
+            sql="INSERT INTO users(userFName,userLName,email,username,address,password)VALUES( % s, % s, % s, % s, % s, % s)"
+            cur.execute(sql,(self.fname, self.lname, self.username, self.email, self.address, self.password))
+            con.commit()
+            msg='User Registration Successful'
+            return {'mesg':msg,'isvalid':True}
+        else:
+            msg = 'Duplicate email, Please use a different email'
+            return {'mesg': msg, 'isvalid': False}
 
-
+    # Function for Validating the uniqueness of the email
+    def validateSignup(self):
+        message = {'supValid': False}
         con = DatabaseConnection.connection
         cur = con.cursor()
-        cur.execute(
-            """INSERT INTO
-                users (
-                    userFName,
-                    userLName,
-                    email,
-                    username,
-                    address,
-                    password)
-            VALUES (%s,%s,%s,%s,%s,%s)""",
-            (self.fname, self.lname, self.username, self.email, self.address, self.password))
-        con.commit()
-        return 'User Registration Successful.'
+        sql = "SELECT * From users WHERE email=%s"
+        row_count = cur.execute(sql,self.email)
+        print('Results Fetched form users table, email validation in progress')
+        if(row_count == 0):
+            print('email entered by user is unique')
+            message = {'supValid': True}
+            return message
+        else:
+            message = {'supValid': False}
+            return message
