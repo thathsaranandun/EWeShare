56389e3fa4911c0cd9cc33ed880322e5139a7174
thathsaranandun <46743657+thathsaranandun@users.noreply.github.com>
2019-05-05 11:21:21 +0530

    Revert "Revert "prediction user input validations were done""

--- client/src/pages/tabs/tabs.ts
+++ client/src/pages/tabs/tabs.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { MusicPage } from '../music/music';
 import { NewsPage } from '../news/news';
 import { VideoPage } from '../video/video';
-import { Profile } from '../profile/profile';
 import { MapPage } from '../map/map';
 import { PredictionPage } from '../prediction/prediction';
 import { AddSitePage } from '../add-site/add-site';
@@ -17,7 +16,7 @@ export class TabsPage {
   tab1Root = MapPage;
   tab2Root = PredictionPage;
   tab3Root = AddSitePage;
-  tab4Root = Profile;
+  tab4Root = PredictionPage;
   tab5Root = ArduinoPage;
 
 
