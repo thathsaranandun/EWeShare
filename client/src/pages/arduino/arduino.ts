479d8f9c916f1fcb96f45c7c6ad7c0d06325b136
thathsaranandun <46743657+thathsaranandun@users.noreply.github.com>
2019-05-05 17:32:41 +0530

    Added update firebase with start stop in charge pg

--- client/src/pages/arduino/arduino.ts
+++ client/src/pages/arduino/arduino.ts
@@ -17,22 +17,58 @@ import { AngularFireDatabase } from 'angularfire2/database';
 export class ArduinoPage {
 
   s;
-  energy_c:any;
-  power_c:any;
+  energy_c:any=0;
+  power_c:number=0;
+  charge:boolean=false;
+  chargerId:number=9;
+  cost:number=100;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,public firebase:AngularFireDatabase) {
-    this.s=this.firebase.object('energy_c').valueChanges().subscribe(data =>{
-      console.log(data);
-      this.energy_c=data;
-    });
-    this.s=this.firebase.object('power_c').valueChanges().subscribe(data =>{
-      console.log(data);
-      this.power_c=data;
-    });
+
+      this.s=this.firebase.object('/'+this.chargerId).valueChanges().subscribe((data:any) =>{
+        console.log(data);
+          this.energy_c=data.energy_c;
+          this.power_c=data.power_c;
+        
+      });
+    
+  }
+
+  start(){
+    this.charge=true;
+    this.firebase.object('/'+this.chargerId).set({
+      charging:this.charge,
+      cost:this.cost,
+      energy_c:0,
+      power_c:0
+
+    })
+
+
+  }
+
+  stop(){
+    this.charge=false;
+    this.firebase.object('/'+this.chargerId).set({
+      charging:this.charge,
+      cost:this.cost,
+      energy_c:this.energy_c,
+      power_c:this.power_c
+    })
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ArduinoPage');
+    this.firebase.object('/'+this.chargerId).set({
+      charging:this.charge,
+      cost:this.cost,
+      energy_c:0,
+      power_c:0
+
+    })
+    console.log('Charging point updated in firebase')
+
   }
 
 }
