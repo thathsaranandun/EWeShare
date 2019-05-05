113c71135e64a2af70611c32cfa75ef71e691e20
thathsaranandun <46743657+thathsaranandun@users.noreply.github.com>
2019-05-05 16:49:28 +0530

    added functionality to start and stop buttons in charge

--- client/src/pages/arduino/arduino.ts
+++ client/src/pages/arduino/arduino.ts
@@ -17,18 +17,43 @@ import { AngularFireDatabase } from 'angularfire2/database';
 export class ArduinoPage {
 
   s;
-  energy_c:any;
-  power_c:any;
+  energy_c:any=0;
+  power_c:number=0;
+  charge:boolean=false;
+  chargerId:number=1;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,public firebase:AngularFireDatabase) {
-    this.s=this.firebase.object('energy_c').valueChanges().subscribe(data =>{
-      console.log(data);
-      this.energy_c=data;
+      this.s=this.firebase.object('/'+this.chargerId).valueChanges().subscribe((data:any) =>{
+        console.log(data);
+        this.energy_c=data.energy_c;
+      });
+      this.s=this.firebase.object('/'+this.chargerId).valueChanges().subscribe((data:any) =>{
+        console.log(data);
+        this.power_c=data.power_c;
     });
-    this.s=this.firebase.object('power_c').valueChanges().subscribe(data =>{
-      console.log(data);
-      this.power_c=data;
-    });
+    
+  }
+
+  start(){
+    this.charge=true
+    this.firebase.object('/'+this.chargerId).set({
+      charging:this.charge,
+      chargerId:this.chargerId,
+      energy_c:0,
+      power_c:0
+
+    })
+
+
+  }
+
+  stop(){
+    this.charge=false
+    this.firebase.object('/'+this.chargerId).set({
+      charging:this.charge,
+      chargerId:this.chargerId
+    })
+
   }
 
   ionViewDidLoad() {
