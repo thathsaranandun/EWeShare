e2b4ff7bab949f412930d7281cb292c367a02237
thathsaranandun <46743657+thathsaranandun@users.noreply.github.com>
2019-05-21 11:00:01 +0530

    added boolean truth update from firebase

--- client/src/pages/arduino/arduino.ts
+++ client/src/pages/arduino/arduino.ts
@@ -22,6 +22,9 @@ export class ArduinoPage {
   charge:boolean=false;
   chargerId:number=9;
   cost:number=0;
+  key:string="";
+  ard_key:string="";
+  truth:boolean;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,public firebase:AngularFireDatabase) {
 
@@ -35,14 +38,34 @@ export class ArduinoPage {
   }
 
   start(){
+    this.firebase.object('/'+this.chargerId).set({
+      charging:this.charge,
+      cost:this.cost,
+      energy_c:0,
+      power_c:0,
+      ard_key:this.ard_key
+
+
+    })
+    
+    this.firebase.object('/'+this.chargerId).valueChanges().subscribe((data:any) =>{
+      console.log(data);
+        this.truth=data.truth;
+      
+    });
+
+    if(this.truth){
     this.charge=true;
     this.firebase.object('/'+this.chargerId).set({
       charging:this.charge,
       cost:this.cost,
       energy_c:0,
-      power_c:0
+      power_c:0,
+      ard_key:this.ard_key
+
 
     })
+  }
 
 
   }
